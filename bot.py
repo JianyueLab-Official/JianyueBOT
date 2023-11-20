141782bf236cac32d34d2257e5169d87274b614d
JianyueHugo <valor@jianyuelab.cc>
2023-11-20 22:18:57 +0900

    v0.0.2-002--change code order

--- bot.py
+++ bot.py
@@ -19,9 +19,16 @@ async def on_message(message):
     if message.content == '!jianyuelab':
         await message.channel.send("JianyueLab is a organization gathered with a group of people who are liking developing, servers, locallize and etc.")
     
-    if message.content == '!help':
-        await message.channel.send("!jianyuelab <- Explain what is JianyueLab \n !changestatus <- change bot status \n !speak <- let bot speak something.")
-    
+    if message.content == '!minecraft':
+        await message.channel.send("Before you playing the server hold by JianyueLab, you need apply for whitelist")
+        
+    if message.content.startswith('!speak'):
+        tmp = message.content.split(" ",1)
+        if len(tmp) == 1:
+            await message.channel.send("What do you want let me to speak?")
+        else:
+            await message.channel.send(tmp[1])
+        
     if message.content.startswith('!changestatus'):
         tmp = message.content.split(" ",1)
         if len(tmp) == 1:
@@ -30,14 +37,7 @@ async def on_message(message):
             game = discord.Game(tmp[1])
             await client.change_presence(status=discord.Status.idle, activity=game)
     
-    if message.content == '!minecraft':
-        await message.channel.send("Before you playing the server hold by JianyueLab, you need apply for whitelist")
-        
-    if message.content.startswith('!speak'):
-      tmp = message.content.split(" ",1)
-      if len(tmp) == 1:
-        await message.channel.send("What do you want let me to speak?")
-      else:
-        await message.channel.send(tmp[1])
+    if message.content == '!help':
+        await message.channel.send("!jianyuelab <- Explain what is JianyueLab \n !changestatus <- change bot status \n !speak <- let bot speak something.")
    
 client.run('MTE3NTQzMDYxODk5NDE4MDIwNg.GLaTtn.SxXOMfkmu1XMMzL6nQreW1NmMogD8lSNOTfNOM')
