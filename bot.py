419433a5ec87b90e54c55653fce48bbf0166f6aa
JianyueHugo <valor@jianyuelab.cc>
2023-11-20 22:10:15 +0900

    v0.0.2

--- bot.py
+++ bot.py
@@ -20,9 +20,24 @@ async def on_message(message):
         await message.channel.send("JianyueLab is a organization gathered with a group of people who are liking developing, servers, locallize and etc.")
     
     if message.content == '!help':
-        await message.channel.send("!jianyuelab <- Explain what is JianyueLab")
+        await message.channel.send("!jianyuelab <- Explain what is JianyueLab \n !changestatus <- change bot status \n !speak <- let bot speak something.")
+    
+    if message.content.startswith('!changestatus'):
+        tmp = message.content.split(" ",2)
+        if len(tmp) == 1:
+            await message.channel.send("What do you want to change?")
+        else:
+            game = discord.Game(tmp[1])
+            await client.change_presence(status=discord.Status.idle, activity=game)
     
     if message.content == '!minecraft':
         await message.channel.send("Before you playing the server hold by JianyueLab, you need apply for whitelist")
+        
+    if message.content.startswith('!speak'):
+      tmp = message.content.split(" ",2)
+      if len(tmp) == 1:
+        await message.channel.send("What do you want let me to speak?")
+      else:
+        await message.channel.send(tmp[1])
    
 client.run('MTE3NTQzMDYxODk5NDE4MDIwNg.GLaTtn.SxXOMfkmu1XMMzL6nQreW1NmMogD8lSNOTfNOM')
