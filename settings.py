d22ee66251a1174a9c76ba73e10c2d0e2f3ffc06
JianyueHugo <valor@jianyuelab.cc>
2024-01-30 10:10:45 +0900

    test

--- settings.py
+++
@@ -1,8 +0,0 @@
-# Discord Bot Token
-TOKEN = 'MTE3NTQzMDYxODk5NDE4MDIwNg.GLaTtn.SxXOMfkmu1XMMzL6nQreW1NmMogD8lSNOTfNOM'
-# Discord Bot Default Status
-default_custom_status = '/help'
-# Discord Bot Default Status Mode: (online, idle, do_not_disturb)
-default_status = 'idle'
-# Do not change this number!
-setting_version = "1"
